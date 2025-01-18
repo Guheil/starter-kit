a7163e14be063ba868b2d5cce6b5674b1a08a2b1
Andriq Klyne Ajido <ajido.andriqklyne@gmail.com>
2025-01-18 22:11:16 +0800

    WORKING NA ANG IMONG NAVIGATION AFTER 293814389 yearssss

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 import { View, StyleSheet } from 'react-native';
 import Home from './src/pages/Home';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 const App: React.FC = () => {
   return (
     <NavigationContainer>
